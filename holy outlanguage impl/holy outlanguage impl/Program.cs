fbc39849b2390f864fd0eae83fb481658712fcd1
Rec <recSdevel@gmail.com>
2026-07-02 18:25:23 +0900

    firstCommit
    project first commit

---
+++ holy outlanguage impl/holy outlanguage impl/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace holy_Outlanguage_impl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
